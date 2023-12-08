9c6b4c654acc496595af371f86f159cd6b0e7337
lpesce57 <113311610+lpesce57@users.noreply.github.com>
2023-12-08 17:49:49 -0500

    Update open_weather_api.py

--- open_weather_api.py
+++ open_weather_api.py
@@ -1,46 +1,38 @@
 import sqlite3
 import matplotlib.pyplot as plt
 import requests
+import json
 from datetime import datetime
 
-def setUpDatabase(db_name):
-	api_key = "INSERT"
-	base_url = "http://api.openweathermap.org/data/2.5/weather?"
-	city_name = "Ann+Arbor"
-	complete_url = f"{base_url}q={city_name}&appid={api_key}"
-	conn = sqlite3.connect("/Users/laurenpesce/Desktop/SI\ 206\ Final\ Project/open_weather.db")
-	cur = conn.cursor()
-	return cur, conn, complete_url
+def setUpDatabase(database):
+    conn = sqlite3.connect(database)
+    cur = conn.cursor()
+    return cur, conn
 
+def create_weather_table(cur, conn):
+    cur.execute("CREATE TABLE IF NOT EXISTS weather (date TEXT PRIMARY KEY, high_temp REAL, low_temp REAL)")
+    conn.commit()
 
-def create_weather_db(cur, conn):
-	cur.execute("CREATE TABLE IF NOT EXISTS weather (temp INTEGER PRIMARY KEY, month INTEGER, day INTEGER, year INTEGER)")
-	conn.commit()
-	
-def add_weather_data(cur, conn, complete_url):
-	response = requests.get(complete_url)
-	weather_data = response.json()
-	if weather_data["cod"] != "404":
-		main_key = weather_data["main"]
-		temp = main_key["temp"]["max"]
-		date = datetime.utcfromtimestamp(weather_data["dt"]).strftime('%Y-%m-%d')
-		month, day, year = map(int, date.split('-'))
-		cur.execute("INSERT OR IGNORE INTO weather (temp, month, date, year) VALUES(?, ?, ?, ?)", (temp, month, day, year))
-		conn.commit()
-	conn.close()
-	
+def add_weather_data(cur, conn):
+    ap_id = "INSERT"
+    url = "https://history.openweathermap.org/data/2.5/history/city?lat=42.279594&lon=-83.732124&type=hour&start=439788694&end=1702074636&appid=(ap_id)"
+    response = requests.get(url)
+    weather_data = response.json()
+
+    if weather_data["cod"] != "404":
+        for data in weather_data:
+            main_data = data["main"]
+            high_temp = main_data["temp"]["max"]
+            low_temp = main_data["temp"]["min"]
+            date = datetime.utcfromtimestamp(data["dt"]).strftime('%Y-%m-%d')
+            cur.execute("INSERT OR IGNORE INTO weather (date, high_temp, low_temp) VALUES(?, ?, ?)", (date, high_temp, low_temp))
+        conn.commit()
 
 def main():
-    # SETUP DATABASE AND TABLE
-	cur, conn = setUpDatabase('open_weather.db')
-	create_weather_db(cur, conn)
-	api_key = "INSERT"
-	base_url = "http://api.openweathermap.org/data/2.5/weather?"
-	city_name = "Ann+Arbor"
-	complete_url = f"{base_url}q={city_name}&appid={api_key}"
-	add_weather_data(cur, conn, complete_url)
-
+    cur, conn = setUpDatabase('open_weather.db')
+    create_weather_table(cur, conn)
+    add_weather_data(cur, conn)
+    conn.close()
 
 if __name__ == "__main__":
     main()
-	
